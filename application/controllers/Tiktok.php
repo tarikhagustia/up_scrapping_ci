17e38d66c9368f785be303bb1f2cf029ffe8c496
Tarikh Agustia Ijudin <agustia.tarikh150@gmail.com>
2020-07-27 19:45:49 +0700

    Add Video Load

--- application/controllers/Tiktok.php
+++ application/controllers/Tiktok.php
@@ -1,23 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Tiktok extends CI_Controller {
+class Tiktok extends CI_Controller
+{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *        http://example.com/index.php/welcome
+     *    - or -
+     *        http://example.com/index.php/welcome/index
+     *    - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
 
     public function __construct()
     {
@@ -42,6 +43,17 @@ class Tiktok extends CI_Controller {
         $url = $this->input->get('url');
         $video = $this->scrapping->tiktokVideo($url);
         $this->load->view('tiktok_video', compact('video'));
+    }
+
+    public function getVideoMeta()
+    {
+        $url = $this->input->get('url');
+
+        $json = $this->scrapping->tiktokVideo($url);
+        return $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(200)
+            ->set_output(json_encode($json));
 
     }
 
