ddf32584d012104ae57b4076ec2fb42d08e0739c
Tarikh Agustia <agustia.tarikh150@gmail.com>
2020-11-20 08:24:33 +0700

    Daily motion add playlist

--- application/controllers/DailyMotionController.php
+++ application/controllers/DailyMotionController.php
@@ -20,10 +20,10 @@ class DailyMotionController extends CI_Controller
     public function index()
     {
         $page = $this->input->get('page') ?? 1;
-        $perPage = $this->input->get('per_page') ?? 9;
+        $perPage = $this->input->get('per_page') ?? 100;//9;
 
         $results = $this->api->get('/me/videos', [
-            'fields' => ['id', 'title', 'channel', 'owner', 'embed_url', 'embed_html', 'thumbnail_url', 'description', 'status'],
+            'fields' => ['id', 'title', 'channel', 'owner', 'embed_url', 'embed_html', 'thumbnail_url', 'description', 'status', 'thumbnail_120_url', 'thumbnail_180_url', 'thumbnail_240_url', 'thumbnail_360_url', 'thumbnail_480_url', 'thumbnail_720_url', 'thumbnail_1080_url'],
             'page'   => $page,
             'limit'  => $perPage,
             // 'owner'  => 'x2fj06o'
@@ -31,5 +31,30 @@ class DailyMotionController extends CI_Controller
 
 
         $this->load->view('daily_motion', compact('results', 'perPage', 'page'));
+    }
+
+    public function playlist()
+    {
+        $page = $this->input->get('page') ?? 1;
+        $perPage = $this->input->get('per_page') ?? 100;//9;
+
+        $results = $this->api->get('/me/playlists', [
+            'fields' => ['id', 'name', 'videos_total', 'description', 'thumbnail_url'],
+            'page'   => $page,
+            'limit'  => $perPage,
+            // 'owner'  => 'x2fj06o'
+        ]);
+
+        foreach ($results['list'] as $key => $item) {
+            // Ambil video
+            $videos = $this->api->get("/playlist/{$item['id']}/videos", [
+                'fields' => ['id', 'title', 'channel', 'owner', 'embed_url', 'embed_html', 'thumbnail_url', 'description', 'status', 'thumbnail_120_url', 'thumbnail_180_url', 'thumbnail_240_url', 'thumbnail_360_url', 'thumbnail_480_url', 'thumbnail_720_url', 'thumbnail_1080_url'],
+            ]);
+
+            $results['list'][$key]['videos'] = $videos['list'];
+        }
+
+
+        $this->load->view('daily_motion_playlist', compact('results', 'perPage', 'page'));
     }
 }
---
+++ application/views/daily_motion_playlist.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<body>
+    <div class="container">
+        <h3>Daily Motion</h3>
+        <p>Total Video : <?= $results['total'] ?></p>
+        <p>Halaman : <?= $page ?></p>
+        <p>Per Halaman : <?= $perPage ?></p>
+        <div class="row">
+            <?php foreach ($results['list'] as $row): ?>
+            <div class="col-sm-4 mb-5">
+                <img src="<?= $row['thumbnail_url'] ?>" class="img-fluid">
+                <h3><?= $row['name'] ?></h3>
+                <p>Description : <?= $row['description'] ?></p>
+                <p>Total Video : <?= $row['videos_total'] ?></p>
+                <ul>
+                    <?php foreach ($row['videos'] as $v): ?>
+                    <li><?= $v['title'] ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+            <?php endforeach; ?>
+        <?php if ($results['has_more']): ?>
+            <a class="btn btn-primary" href="<?= base_url('dailymotioncontroller') ?>?page=<?= $page + 1 ?>">Halaman Selanjutnya</a>
+        <?php endif ?>
+        </div>
+    </div>
+</body>
+</html>
